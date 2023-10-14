0e631fa0658909ab1633c7a7d0d4ed41f2c95453
jkea1 <jinkyng7890@gmail.com>
2023-10-14 20:20:24 +0900

    feat: layout 스타일링

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <p>
+    <p className='bg-slate-900 text-white py-2 text-sm text-center'>
       프론트엔드 개발 공부 과정을 기록하는 블로그 입니다. | All Right Reserved
     </p>
   )
